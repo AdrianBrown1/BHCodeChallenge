8d5ae22cb27ba4e32239009bbbab1760bf5a25f5
Adrian Brown <adrianb@bighuman.com>
2017-04-25 15:27:06 -0400

    Task: coreData setup

--- TrackingISS/TrackingISS/CoreDataStack.swift
+++ TrackingISS/TrackingISS/CoreDataStack.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    private static let name = "PinModel"
+
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.name)
+        
+        container.loadPersistentStores { storeDescription, error in
+        
+            if let error = error as NSError? {
+                print(error)
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges{
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                print("Unresolved error: \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    
+}
+
+extension Pin {
+    class var fetch: NSFetchRequest<Pin> {
+        do {
+            return NSFetchRequest<Pin>(entityName: "Pin")
+        }
+   
+    }
+    
+    public override var description: String {
+        
+        return "Pins name \(name) lat: \(latitude) long: \(longitude)"
+        
+    }
+    
+    
+    
+}
