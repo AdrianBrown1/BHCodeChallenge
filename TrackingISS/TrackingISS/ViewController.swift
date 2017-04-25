a342f3cd6788e5ef913e9d253d1868cfdb9c1207
Adrian Brown <adrianb@bighuman.com>
2017-04-25 15:29:12 -0400

    Initial setup

--- TrackingISS/TrackingISS/ViewController.swift
+++ TrackingISS/TrackingISS/ViewController.swift
@@ -11,34 +11,32 @@ import MapKit
 import CoreLocation
 
 
+
+
 class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDelegate {
   
     var locationManager: CLLocationManager!
     var latitude: Double?
     var longitude: Double?
-    let satellitePin = MKPointAnnotation()
-    var longPressWasHit: Bool = false
+    var satelliteMapPin = SatelliteMapPin(coordinate: CLLocationCoordinate2D(latitude: 0, longitude:0), title: "ISS Satellite")
+
+    //core Data
+    let coreDataStack = CoreDataStack.shared
     
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         Timer.scheduledTimer(timeInterval: 3.0, target:self, selector: #selector(loadCurrentLocation), userInfo: nil, repeats: true)
-        
-        
-
-       
-       
+ 
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
         satellitePinSetup()
-
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,139 +47,176 @@ class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDeleg
     
     func satellitePinSetup(){
 
+        //Map setup
         mapView.delegate = self
-        mapView.addAnnotation(satellitePin)
+        mapView.showsUserLocation = true
+      
+        // coreData saved pins
+        let pins = try! coreDataStack.context.fetch(Pin.fetch)
+        print(pins.count)
+        if pins.count > 0 {
         
-        satellitePin.title = "ISS Satelite"
+            for pin in pins {
+
+                let pinLat = Double(pin.latitude!)
+                let pinLong = Double(pin.longitude!)
+                
+                let markerPin = MarkerPin(coordinate: CLLocationCoordinate2D(latitude: pinLat!, longitude: pinLong!))
+             
+                
+                mapView.addAnnotation(markerPin)
+        
+            }
+            
         mapView.showAnnotations(mapView.annotations, animated: true)
+  
+        }
         
+        // get the particular pin that was tapped
+        let pinToZoomOn = locationManager.location?.coordinate
         
-        let pinToZoomOn = mapView.annotations[0]
         // optionally you can set your own boundaries of the zoom
-        let span = MKCoordinateSpanMake(150.0, 150.0)
+        let span = MKCoordinateSpanMake(30.0, 30.0)
+        
         // now move the map
-        let region = MKCoordinateRegion(center: pinToZoomOn.coordinate, span: span)
-        mapView.setRegion(region, animated: true)
+        let region = MKCoordinateRegion(center: pinToZoomOn!, span: span)
+        mapView.setRegion(region, animated: true);
         
         let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.handleLongPress(_:)))
         longPressRecogniser.minimumPressDuration = 0.5
         mapView.addGestureRecognizer(longPressRecogniser)
-
-
-   
-        
+ 
     }
+    
+    
     
     
     func handleLongPress(_ getstureRecognizer : UIGestureRecognizer){
+        
         if getstureRecognizer.state != .began { return }
-        
         print("I was tapped")
+        let alert = UIAlertController(title: "Pin's Name", message: "Please enter name for pin.", preferredStyle: .alert)
         
-        let touchPoint = getstureRecognizer.location(in: mapView)
-        let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = newCoordinates
-        annotation.title = "my pin"
-        mapView.addAnnotation(annotation)
-        self.longPressWasHit = true
         
-       
-        
-    }
-    
-    
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-       
-        
-        if longPressWasHit == true {
-
-            if !(annotation is MKPointAnnotation) {
-                return nil
-            }
-            let annotation = MKPointAnnotation()
-            
-            let annotationIdentifier = "pin"
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
-            
-            if annotationView == nil {
-                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-                annotationView!.canShowCallout = true
-            }
-            else {
-                annotationView!.annotation = annotation
-            }
-            let pinImage = UIImage(named: "pin")
-            annotationView!.image = pinImage
-
-            return annotationView
-            
-        }else {
-            
-            if !(annotation is MKPointAnnotation) {
-                return nil
-            }
-            
-            let annotationIdentifier = "satillite"
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
-            
-            if annotationView == nil {
-                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-                annotationView!.canShowCallout = true
-            }
-            else {
-                annotationView!.annotation = annotation
-            }
-            
-            let pinImage = UIImage(named: "satillite")
-            annotationView!.image = pinImage
-            
-            return annotationView
-
+        let touchPoint = getstureRecognizer.location(in: self.mapView)
+        let newCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+        let markerPin = MarkerPin(coordinate: newCoordinates)
+        markerPin.coordinate = newCoordinates
+        alert.addTextField { (textField) in
+            //
         }
         
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+            markerPin.title = textField?.text
+            self.mapView.addAnnotation(markerPin)
+
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+
+        let pin = Pin(context: self.coreDataStack.context)
+        let latitude = String(newCoordinates.latitude)
+        let longitude = String(newCoordinates.longitude)
+        pin.latitude = latitude
+        pin.longitude = longitude
+        pin.name = markerPin.title
+        
+        //   use this to save
+       coreDataStack.saveContext()
+ 
+    }
+    
+    func namePinAlert(){
+       
+    }
+ 
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        switch annotation {
+        case is SatelliteMapPin:
+            
+            if let reusedView = mapView.dequeueReusableAnnotationView(withIdentifier: SatelliteMapPin.annotationViewIdentifier) {
+                reusedView.annotation = annotation
+                annotationView = reusedView
+            } else {
+                
+              
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: SatelliteMapPin.annotationViewIdentifier)
+                annotationView?.image = UIImage(named: "satellite")
+            }
+            break
+        case is MarkerPin:
+            if let reusedView = mapView.dequeueReusableAnnotationView(withIdentifier: MarkerPin.annotationViewIdentifier) {
+                reusedView.annotation = annotation
+                annotationView = reusedView
+            } else {
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: MarkerPin.annotationViewIdentifier)
+                annotationView?.image = UIImage(named: "pin")
+            }
+            break
+       
+        default:
+            print("i am nil")
+            return nil
+            
+        }
+        
+        return annotationView
         
     }
     
     func mapView(_: MKMapView, didSelect view: MKAnnotationView) {
-        
         // get the particular pin that was tapped
         let pinToZoomOn = view.annotation
         
         // optionally you can set your own boundaries of the zoom
-        let span = MKCoordinateSpanMake(25.0, 25.0)
+        let span = MKCoordinateSpanMake(55.0, 55.0)
         
         // now move the map
         let region = MKCoordinateRegion(center: pinToZoomOn!.coordinate, span: span)
         mapView.setRegion(region, animated: true)
     }
     
-
-    
     func loadCurrentLocation(){
-
-        issClient.getISSCurrentLocation { (dictionary, error) in
+        //ISS Satellite Setup
+        issClient.getISSCurrentLocation { [weak self] (dictionary, error) in
+            
             if issClient.satellite?.latitude != nil && issClient.satellite?.longitude != nil {
                 
-                let lat = Double((issClient.satellite?.latitude)!)
-                let long = Double((issClient.satellite?.longitude)!)
-                let satelliteLocation = CLLocationCoordinate2DMake(lat!, long!)
+                let lat = Double((issClient.satellite?.latitude)!)!
+                let long = Double((issClient.satellite?.longitude)!)!
+                let satelliteLocation = CLLocationCoordinate2DMake(lat, long)
                 
-                //Set satellite location
-                self.satellitePin.coordinate = satelliteLocation
-             
+                self?.satelliteMapPin.coordinate = satelliteLocation
+                
+                self?.mapView.removeAnnotation((self?.satelliteMapPin)!)
+                self?.mapView.addAnnotation((self?.satelliteMapPin)!)
+                
             }
-
-            
         }
+        
         
     }
     
+    @IBAction func zoomToSatellite(_ sender: Any) {
     
+        let pinToZoomOn = satelliteMapPin.coordinate
+        
+        // optionally you can set your own boundaries of the zoom
+        let span = MKCoordinateSpanMake(25.0, 25.0)
+        
+        // now move the map
+        let region = MKCoordinateRegion(center: pinToZoomOn, span: span)
+        mapView.setRegion(region, animated: true);
+        print("Find the fucking ISS")
     }
-
     
-
-
+    
+}
 
