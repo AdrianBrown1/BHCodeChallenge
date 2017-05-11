9684ac0e4eb5683b2ed9018418c322d8918f49f6
Adrian Brown <adrianb@bighuman.com>
2017-05-11 14:17:48 -0400

    chore: refactor setup

--- TrackingISS/IssClient.swift
+++ TrackingISS/IssClient.swift
@@ -11,27 +11,17 @@ import Alamofire
 
 
 class issClient {
-    
-    static var satellite: ISSSatellite?
-    
+  
     static func getISSCurrentLocation( completionHandler: @escaping ([String: Any]?, Error?) -> ()) {
 
         let issLocationURL = "http://api.open-notify.org/iss-now.json"
-        
+
         Alamofire.request(issLocationURL).responseJSON { response in
             
             switch response.result {
             case.success(let value):
-               
                 completionHandler(value as? NSDictionary as! [String : Any]?, nil)
-                let dictionary: Dictionary = value as! [String: Any]
-                let position: Dictionary  = dictionary["iss_position"] as! [String: String]
-                let lat = position["latitude"]
-                let long = position["longitude"]
-                let timeStamp = dictionary["timestamp"]
-                
-                self.satellite = ISSSatellite.init(timeStamp: timeStamp! as! NSNumber, latitude: lat!, longitude: long!)
-                
+           
             case .failure(let error):
                 completionHandler(nil, error)
 
@@ -42,24 +32,22 @@ class issClient {
     
     }
     
-    static func getNextPassTime(lattitude: Double, longitude: Double, completionHandler: @escaping([String: Any]?, Error?) -> ()) {
+    static func getNextPassTime(lattitude: Double, longitude: Double,pin: Pin, completionHandler: @escaping([String: Any]?, Error?) -> ()) {
         
         let passTimeURL = "http://api.open-notify.org/iss-pass.json?lat=\(lattitude)&lon=\(longitude)"
-        
-        Alamofire.request(passTimeURL).responseJSON { response in
-            
-            
-            if let JSON = response.result.value {
-                print("JSON: \(JSON)")
+        let queue = DispatchQueue(label: "BackgroundQueue", qos: .background, attributes: .concurrent)
+
+        Alamofire.request(passTimeURL).responseJSON(queue: queue) { response in
                 
-                
+                switch response.result {
+                case.success(let value):
+                    completionHandler(value as? NSDictionary as! [String : Any]?, nil)
+    
+                case .failure(let error):
+                    completionHandler(nil, error)
+                }
                 
             }
-        }
-        
-        
-        
-        
     }
     
     
