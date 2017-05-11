5af29182c816631115bda07bade3c4260f789772
Adrian Brown <adrianb@bighuman.com>
2017-05-11 14:19:33 -0400

    task: initial setup

--- TrackingISS/TrackingISS/MarkerPin.swift
+++ TrackingISS/TrackingISS/MarkerPin.swift
@@ -13,8 +13,7 @@ class MarkerPin: NSObject, MKAnnotation {
     
     static var annotationViewIdentifier = "pin"
     var coordinate: CLLocationCoordinate2D
-    var title: String?
-    var subtitle: String?
+
     
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
