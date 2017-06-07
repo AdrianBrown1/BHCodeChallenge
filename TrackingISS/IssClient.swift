ff26344ed53f94de47cf379506e593e68eed98c1
Adrian Brown <adrianb@bighuman.com>
2017-06-07 17:11:45 -0400

    chore: revision

--- TrackingISS/IssClient.swift
+++ TrackingISS/IssClient.swift
@@ -9,47 +9,31 @@
 import Foundation
 import Alamofire
 
-
 class issClient {
-  
+    
     static func getISSCurrentLocation( completionHandler: @escaping ([String: Any]?, Error?) -> ()) {
-
-        let issLocationURL = "http://api.open-notify.org/iss-now.json"
-
-        Alamofire.request(issLocationURL).responseJSON { response in
-            
+        
+        Alamofire.request(Constants.baseURL + Constants.locationURL).responseJSON { response in
             switch response.result {
             case.success(let value):
                 completionHandler(value as? NSDictionary as! [String : Any]?, nil)
-           
             case .failure(let error):
                 completionHandler(nil, error)
-
             }
- 
-      
         }
-    
     }
     
     static func getNextPassTime(lattitude: Double, longitude: Double,pin: Pin, completionHandler: @escaping([String: Any]?, Error?) -> ()) {
         
-        let passTimeURL = "http://api.open-notify.org/iss-pass.json?lat=\(lattitude)&lon=\(longitude)"
+        let passTimeURL = "\(Constants.baseURL)\(Constants.passTimeURL)lat=\(lattitude)&lon=\(longitude)"
         let queue = DispatchQueue(label: "BackgroundQueue", qos: .background, attributes: .concurrent)
-
         Alamofire.request(passTimeURL).responseJSON(queue: queue) { response in
-                
-                switch response.result {
-                case.success(let value):
-                    completionHandler(value as? NSDictionary as! [String : Any]?, nil)
-    
-                case .failure(let error):
-                    completionHandler(nil, error)
-                }
-                
+            switch response.result {
+            case.success(let value):
+                completionHandler(value as? NSDictionary as! [String : Any]?, nil)
+            case .failure(let error):
+                completionHandler(nil, error)
             }
+        }
     }
-    
-    
-    
 }
---
+++ TrackingISS/TrackingISS/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TrackingISS
+//
+//  Created by Adrian Brown on 6/5/17.
+//  Copyright © 2017 Adrian Brown. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
